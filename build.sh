c4d5e5b2b611a54cc90150863dd0e946c0567b1e
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2017-01-01 02:05:04 -0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+case $1 in
+    osx)
+        NASMFLAGS="-fmacho32 -DOSX"
+        LDFFAGS=""
+        ;;
+    linux)
+        NASMFLAGS="-felf32"
+        LDFLAGS="-m elf_i386"
+        ;;
+    *)
+        echo 'usage: ./build.sh [osx|linux]'
+        exit 1
+esac
+
+rm -f aforth && nasm $NASMFLAGS -o aforth.o aforth.S && ld $LDFLAGS -o aforth aforth.o && echo 'aforth build Ok' && exit 0
+
+echo 'aforth build FAILED'
+exit 1
